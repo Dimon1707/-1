22752610edcaaa1b703093126217e885889fcdc1
Dimon1707 <81287959+Dimon1707@users.noreply.github.com>
2021-05-01 14:49:52 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-a= int(input())
-b= int(input())
-if a>b:
-    print(a)
-else:
-    print(b)
+a = int(input())
+b = int(input())
+    if a > b:
+        print(1)
+    if a < b:
+        print(2)
+    if a == b:
+        print(0)
