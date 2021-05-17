692f54ab9ab7d11a6dc700aa03fba52943cf7e6e
Dimon1707 <81287959+Dimon1707@users.noreply.github.com>
2021-05-17 22:17:33 +1000

    Add files via upload

--- Class_Car.py
+++ Class_Car.py
@@ -5,17 +5,16 @@ class Car():
         self.Gas = Gas
         self.Vgas = Vgas
 
-    def zapravka(self):
-        int(self.Vgas)
-        int(self.Gas)
-        if self.Vgas > self.Gas:
-            self.Gas += 10
+    def zapravka(self, Benz):
+        self.Benz = Benz
+        if self.Vgas > self.Gas + Benz:
+            self.Gas = self.Gas + Benz
             print('В баке', self.Gas, ' литров, а обём', self.Vgas, ' и можно заправлять')
         else:
             print('Обём бака', self.Vgas, ' а хотите налить ещё, когда там', self.Gas, 'литров и он переполнен')
 
 
-Pogani_Huyara = Car('Moskva', '50 веков', 100, 50)
+Pogani_Huyara = Car('Moskva', '50 веков', 10, 50)
 print(Pogani_Huyara.cuntry)
 print(Pogani_Huyara.year)
-Pogani_Huyara.zapravka()
+Pogani_Huyara.zapravka(30)
